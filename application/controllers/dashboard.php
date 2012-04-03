d727334812ac8b5e8e7ce8ed252c830403f42927
Joseph D. Purcell <josephdpurcell@gmail.com>
2012-04-02 21:47:29 -0400

    Add base dashboard controller and views.

---
+++ application/controllers/dashboard.php
@@ -0,0 +1,14 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('dashboard/header');
+		$this->load->view('dashboard/index');
+		$this->load->view('dashboard/footer');
+	}
+}
+
+/* End of file dashboard.php */
+/* Location: ./application/controllers/dashboard.php */
---
+++ application/views/dashboard/index.php
@@ -0,0 +1,6 @@
+	<h1>MyFedha</h1>
+
+	<div id="body">
+        <p>This is the dashboard</p>
+	</div>
+
