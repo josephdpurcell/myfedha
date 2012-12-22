7b35782915f53c891afa343a77592a6518b7f666
Joseph D. Purcell <josephdpurcell@gmail.com>
2012-12-22 15:23:36 -0500

    Another step closer.

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -5,6 +5,36 @@ class Api extends CI_Controller {
     public function __construct ()
     {
         parent::__construct();
+
+		// restrict if we need to
+		$restricted_paths = array('import');
+		if (in_array($this->uri->segment(2),$restricted_paths)) {
+			$this->user->restrict();
+			$this->load->model('transaction');
+			$this->load->model('account');
+
+			// load account
+			if ($this->uri->segment(3)) {
+				// get account
+				try {
+					$this->account->load_account($this->uri->segment(3));
+				} catch (Exception $e) {
+					// user has no accounts
+					$this->session->set_flashdata('error',$e->getMessage());
+					header("Location: /accounts");
+				}
+			} else {
+				// get default account
+				try {
+					$this->account->load_account();
+				} catch (Exception $e) {
+					// user has no accounts
+					$this->session->set_flashdata('error',$e->getMessage());
+					header("Location: /accounts");
+				}
+			}
+		}
+
 		header('Content-Type: application/json');
     }
 
@@ -15,11 +45,43 @@ class Api extends CI_Controller {
 
 	public function upload()
 	{
+		// initialize upload handler (and process upload)
 		require('UploadHandler.php');
-		$upload_handler = new UploadHandler(array(
+		$options = array(
 			'user_dirs' => true,
 			'download_via_php' => true
-		));
+		);
+		$initialize = true;
+		$upload_handler = new UploadHandler($options, $initialize);
+	}
+
+	public function import()
+	{
+		// initialize upload handler
+		require('UploadHandler.php');
+		$options = array(
+			'user_dirs' => true,
+			'download_via_php' => true
+		);
+		$initialize = false;
+		$upload_handler = new UploadHandler($options, $initialize);
+
+		// read file
+		$file = current($upload_handler->get(false));
+		$file = 'files/'.session_id().'/'.$file->name;
+		if (file_exists($file)) {
+			$contents = file_get_contents($file);
+			$contents = json_decode($contents, true);
+			foreach ($contents['transactions'] as $i=>$c) {
+				echo $i.') '.$c['description']."\n";
+			}
+			$retval = true;
+		} else {
+			$retval = false;
+		}
+
+		echo json_encode($retval);
+		exit;
 	}
 
 }
--- application/views/import/index.php
+++ application/views/import/index.php
@@ -5,87 +5,75 @@
         <input name="add" type="submit" class="submit" value="Add">
         </form>
 
-
-    <form id="fileupload" action="/api/upload" method="POST" enctype="multipart/form-data">
-        <!-- Redirect browsers with JavaScript disabled to the origin page -->
-        <noscript><input type="hidden" name="redirect" value="http://blueimp.github.com/jQuery-File-Upload/"></noscript>
-        <!-- The fileupload-buttonbar contains buttons to add/delete files and start/cancel the upload -->
-        <div class="row fileupload-buttonbar">
-            <div class="span7">
-                <!-- The fileinput-button span is used to style the file input field as button -->
-                <span class="btn btn-success fileinput-button">
-                    <i class="icon-plus icon-white"></i>
-                    <span>Add files...</span>
-                    <input type="file" name="files[]" multiple>
-                </span>
-                <button type="submit" class="btn btn-primary start">
-                    <i class="icon-upload icon-white"></i>
-                    <span>Start upload</span>
-                </button>
-                <button type="reset" class="btn btn-warning cancel">
-                    <i class="icon-ban-circle icon-white"></i>
-                    <span>Cancel upload</span>
-                </button>
-                <button type="button" class="btn btn-danger delete">
-                    <i class="icon-trash icon-white"></i>
-                    <span>Delete</span>
-                </button>
-                <input type="checkbox" class="toggle">
-            </div>
-            <!-- The global progress information -->
-            <div class="span5 fileupload-progress fade">
-                <!-- The global progress bar -->
-                <div class="progress progress-success progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100">
-                    <div class="bar" style="width:0%;"></div>
-                </div>
-                <!-- The extended global progress information -->
-                <div class="progress-extended">&nbsp;</div>
-            </div>
-        </div>
-        <!-- The loading indicator is shown during file processing -->
-        <div class="fileupload-loading"></div>
-        <br>
-        <!-- The table listing the files available for upload/download -->
-        <table role="presentation" class="table table-striped"><tbody class="files" data-toggle="modal-gallery" data-target="#modal-gallery"></tbody></table>
-    </form>
-
-		<?php
-		/*
-        <table width="100%" border="0" cellpadding="0" cellspacing="0">
-            
-            <tr>
-            
-                <th width="5%" scope="col">
-                <input type="checkbox" name="checkbox" id="checkbox" class="checkall" /><label for="checkbox"></label></th>
-                
-                <th width="18%" scope="col">Date</th>
-                <th width="16%" scope="col">Amount</th>
-                <th width="29%" scope="col">Description</th>
-                <th width="13%" scope="col">Tags</th>
-                <th width="11%" scope="col">Status</th>
-                <th width="8%" scope="col">Action</th>
-            </tr>
-
-            <?php foreach ($transactions as $t) { ?>
-            <tr>
-                <td scope="col"><input type="checkbox" name="checkbox2" id="checkbox2" /></td>
-                <td scope="col"><?php echo date("n/j/y",strtotime($t->date)); ?> </td>
-                <td scope="col"><?php echo $t->amount; ?> </td>
-                <td scope="col"><?php echo $t->description; ?> </td>
-                <td scope="col"><?php echo $t->tags; ?> </td>
-                <td scope="col"><?php echo $t->status; ?> </td>
-                <td scope="col">
-                    <a href="/transactions/delete/<?php echo $account->slug; ?>/<?php echo $t->transaction_id; ?>"><img src="/images/icondock/delete.png" alt="Delete" width="16" height="16"></a>
-                    <a href="/transactions/edit/<?php echo $account->slug; ?>/<?php echo $t->transaction_id; ?>"><img src="/images/icondock/16x16/to_do_list.png" alt="Edit" width="16" height="16"></a>
-                </td>
-            </tr>
-            <?php } ?>
-
-        </table>
-		*/
-		?>
+		<form id="fileupload" action="/api/upload" method="POST" enctype="multipart/form-data">
+			<!-- Redirect browsers with JavaScript disabled to the origin page -->
+			<noscript><input type="hidden" name="redirect" value="http://blueimp.github.com/jQuery-File-Upload/"></noscript>
+			<!-- The fileupload-buttonbar contains buttons to add/delete files and start/cancel the upload -->
+			<div class="row fileupload-buttonbar">
+				<div class="span7">
+					<!-- The fileinput-button span is used to style the file input field as button -->
+					<span class="btn btn-success fileinput-button">
+						<i class="icon-plus icon-white"></i>
+						<span>Add files...</span>
+						<input type="file" name="files[]" multiple>
+					</span>
+					<button type="submit" class="btn btn-primary start">
+						<i class="icon-upload icon-white"></i>
+						<span>Start upload</span>
+					</button>
+					<button type="reset" class="btn btn-warning cancel">
+						<i class="icon-ban-circle icon-white"></i>
+						<span>Cancel upload</span>
+					</button>
+					<button type="button" class="btn btn-danger delete">
+						<i class="icon-trash icon-white"></i>
+						<span>Delete</span>
+					</button>
+					<input type="checkbox" class="toggle">
+				</div>
+				<!-- The global progress information -->
+				<div class="span5 fileupload-progress fade">
+					<!-- The global progress bar -->
+					<div class="progress progress-success progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100">
+						<div class="bar" style="width:0%;"></div>
+					</div>
+					<!-- The extended global progress information -->
+					<div class="progress-extended">&nbsp;</div>
+				</div>
+			</div>
+			<!-- The loading indicator is shown during file processing -->
+			<div class="fileupload-loading"></div>
+			<br>
+			<!-- The table listing the files available for upload/download -->
+			<table role="presentation" class="table table-striped"><tbody class="files" data-toggle="modal-gallery" data-target="#modal-gallery"></tbody></table>
+		</form>
 
 	</div>
+
+<script>
+$(document).ready(function(){
+	$('#thisisme').live('click',function(){
+		file = $(this).data('url');
+		$.ajax({
+			url: '/api/import',
+			async: true,
+			data: {
+				file: file
+			},
+			dataType: 'json',
+			success: function(response){
+				console.log('success');
+				console.log(response);
+			},
+			failure: function(response){
+				console.log('err');
+				console.log(response);
+			},
+		});
+		return false;
+	});
+});
+</script>
 
 <script id="template-upload" type="text/x-tmpl">
 {% for (var i=0, file; file=o.files[i]; i++) { %}
@@ -136,7 +124,7 @@
             <td class="size"><span>{%=o.formatFileSize(file.size)%}</span></td>
             <td></td>
 			<td class="import">
-				<button class="btn btn-primary" data-url="{%=file.url%}">
+				<button id="thisisme" class="btn btn-primary" data-url="{%=file.name%}">
 					<i class="icon-circle-arrow-up icon-white"></i>
 					<span>Import</span>
 				</button>
