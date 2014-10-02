2c7c2df33769f452b1bbe55f56fd7469b8842496
Joseph D. Purcell <josephdpurcell@gmail.com>
2014-10-02 00:50:33 -0400

    WIP

--- html/api/index.php
+++ html/api/index.php
@@ -34,12 +34,102 @@ class Transaction extends \Illuminate\Database\Eloquent\Model
 {
 }
 
+/**
+ * User Model
+ */
+class User extends \Illuminate\Database\Eloquent\Model
+{
+}
+
+class OAuth2Auth extends \Slim\Middleware
+{
+    protected $headers = array();
+
+    public function __construct($headers) {
+        $this->headers = $headers;
+    }
+
+    public function call() {
+        // allow request to /authenticate
+        if (!$this->app->request->isPost() || $this->app->request->getResourceUri()!='/authenticate') {
+            // no auth header
+            try {
+                if (!isset($this->headers['Authorization'])) {
+                    throw new \Exception('No Authentication Header Set', 401);
+                } else {
+                    $authHeader = $this->headers['Authorization'];
+                    $authHeader = strtolower($authHeader);
+                    list($realm, $access_token) = split(' ', $authHeader);
+                    if ($realm != 'oauth') {
+                        throw new \Exception('Invalid Authentication Realm: '.$realm, 400);
+                    }
+                    if (empty($access_token)) {
+                        throw new \Exception('No Access Token Sent', 400);
+                    }
+                    $user = User::where('access_token', $access_token)->first();
+                    if (empty($user)) {
+                        throw new \Exception('Invalid Access Token', 403);
+                    }
+                    $this->app->user = $user;
+                }
+            } catch (\Exception $e) {
+                $this->app->error($e);
+                exit;
+            }
+        }
+        // this line is required for the application to proceed
+        $this->next->call();
+    }
+}
 
 $app = new \Slim\Slim(array(
-    'debug' => true
+    'debug' => false
 ));
 
 $app->response->headers->set('Content-Type', 'application/json');
+
+$app->error(function($e) use ($app) {
+    header((string) $e->getMessage(), true, (int) $e->getCode());
+    echo json_encode(array(
+        'error' => $e->getCode(),
+        'message' => $e->getMessage()
+    ));
+    exit;
+});
+
+$headers = apache_request_headers();
+$app->add(new OAuth2Auth($headers));
+
+$app->post('/authenticate', function () {
+    global $app;
+    $body = $app->request->getBody();
+    $data = json_decode($body);
+    if (empty($data->username)) {
+        throw new \Exception('No Username Sent', 401);
+    } elseif (empty($data->password)) {
+        throw new \Exception('No Password Sent', 401);
+    }
+    $username = $data->username;
+    $password = hash_hmac('sha256', $data->password, 'cryhynInGheelj5!DrodIlgevheigodd');
+    $user = User::where('username', $username)->where('password', $password)->first();
+    if (empty($user)) {
+        throw new \Exception('Incorrect Username or Password', 401);
+    }
+    if (empty($user->access_token)) {
+        $user->access_token = bin2hex(openssl_random_pseudo_bytes(16));
+    }
+    $user->save();
+    echo $user->toJson();
+});
+
+$app->get('/users/:id', function ($id) {
+    global $app;
+    if ($id==$app->user->id) {
+        echo $app->user->toJson();
+    } else {
+        throw new \Exception('Invalid user id sent');
+    }
+});
 
 $app->get('/transaction', function () {
     global $app;
