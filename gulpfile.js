fc10c17c63ef9d8afef97130dac24a0b98664f87
Joseph D. Purcell <josephdpurcell@gmail.com>
2014-10-22 23:21:46 -0500

    Move bower source to src dir.

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,15 @@ var gulp = require('gulp'),
   plumber = require('gulp-plumber');
 
 var vendorjs = [
-  'html/js/vendor/jquery/dist/jquery.js',
-  //'html/js/vendor/q/q.js',
-  'html/js/vendor/localforage/dist/localforage.js',
-  'html/js/vendor/moment/moment.js',
-  'html/js/vendor/URIjs/src/URI.js',
-  'html/js/vendor/angular/angular.js',
-  'html/js/vendor/angular-hotkeys/build/hotkeys.js',
-  'html/js/vendor/angular-localforage/dist/angular-localForage.js',
-  'html/js/vendor/ui-router/release/angular-ui-router.js'
+  'src/js/vendor/jquery/dist/jquery.js',
+  //'src/js/vendor/q/q.js',
+  'src/js/vendor/localforage/dist/localforage.js',
+  'src/js/vendor/moment/moment.js',
+  'src/js/vendor/URIjs/src/URI.js',
+  'src/js/vendor/angular/angular.js',
+  'src/js/vendor/angular-hotkeys/build/hotkeys.js',
+  'src/js/vendor/angular-localforage/dist/angular-localForage.js',
+  'src/js/vendor/ui-router/release/angular-ui-router.js'
 ];
 
 var vendorcss = [
