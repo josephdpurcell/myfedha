dad9eaea76ad7c275a8c38fdfcdd588faba063f1
Joseph D. Purcell <josephdpurcell@gmail.com>
2014-10-25 11:43:38 -0500

    Add moment-recur.

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,15 @@ var gulp = require('gulp'),
   plumber = require('gulp-plumber');
 
 var vendorjs = [
-  'src/js/vendor/jquery/dist/jquery.js',
-  //'src/js/vendor/q/q.js',
-  'src/js/vendor/localforage/dist/localforage.js',
-  'src/js/vendor/moment/moment.js',
   'src/js/vendor/URIjs/src/URI.js',
-  'src/js/vendor/angular/angular.js',
   'src/js/vendor/angular-hotkeys/build/hotkeys.js',
   'src/js/vendor/angular-localforage/dist/angular-localForage.js',
+  'src/js/vendor/angular/angular.js',
+  'src/js/vendor/jquery/dist/jquery.js',
+  'src/js/vendor/localforage/dist/localforage.js',
+  'src/js/vendor/moment-recur/moment-recur.js',
+  'src/js/vendor/moment/moment.js',
+  //'src/js/vendor/q/q.js',
   'src/js/vendor/ui-router/release/angular-ui-router.js'
 ];
 
@@ -25,6 +26,12 @@ gulp.task('minifyjs', function () {
   gulp.src(vendorjs)
     .pipe(concat('vendor.js'))
     .pipe(uglify())
+    .pipe(gulp.dest('html/js'));
+});
+
+gulp.task('concatjs', function () {
+  gulp.src(vendorjs)
+    .pipe(concat('vendor.js'))
     .pipe(gulp.dest('html/js'));
 });
 
