c0687430ec20bf55732be91b0c6cedc23d36d208
Joseph D. Purcell <josephdpurcell@gmail.com>
2014-12-13 15:32:18 +0000

    Bump goal to 600/mo.

--- src/js/all.js
+++ src/js/all.js
@@ -321,7 +321,7 @@ angular.module('myfedha', [
             // Goal per day
             // Goal per week
             // Goal per month
-            var goalPerMonth = 500;
+            var goalPerMonth = 600;
             var goalPerDay = goalPerMonth / endOfMonth.format('D');
             var goalPerWeek = 7 * goalPerDay;
 
